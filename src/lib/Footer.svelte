af2217b7f17b98432927d38963064b39a4dfa624
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-03 07:28:26 +0100

    fix: favicons

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -7,7 +7,7 @@
     <div class="sm:flex sm:items-center sm:justify-between">
         <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse z-10">
       
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LexiLearn Network</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LexiLearn Hub</span>
         </a>
         <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 z-10">
           <li>
