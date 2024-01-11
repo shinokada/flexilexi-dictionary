e115c6cc6a4344a839581fab703b9ee37919d1af
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-11 19:08:41 +0100

    fix: revert Nav

--- src/routes/components/Nav.svelte
+++ src/routes/components/Nav.svelte
@@ -1,26 +1,48 @@
 <script>
-	import { Navbar, NavLi, NavBrand, NavUl, uiHelpers } from 'svelte-5-ui-lib';
-
-	let nav = uiHelpers();
-
-	let navStatus = $state();
-	let toggleNav = nav.toggleNav;
-	let closeNav = nav.closeNav;
-
-	$effect(() => {
-		// this can be done adding nav.navStatus directly to DOM element
-		// without using effect
-		navStatus = nav.navStatus;
-	});
-</script>
-
-<Navbar {toggleNav} {closeNav} {navStatus}>
-	{#snippet brand()}
-		<NavBrand siteName="FlexiLexi dictionary" {closeNav} />
-	{/snippet}
-	<NavUl>
-		<NavLi href="/" {closeNav}>Norsk</NavLi>
-		<NavLi href="/english" {closeNav}>English</NavLi>
-		<NavLi href="/japanese" {closeNav}>Japanese</NavLi>
-	</NavUl>
-</Navbar>
+	import { page } from '$app/stores';
+	let navStatus  = $state(false)
+	let navClass = $derived(
+	  navStatus ? 'block':'hidden'
+	)
+  
+	const toggleNav = ()=>{
+	  // console.log('clicked')
+	  navStatus = !navStatus
+	  // console.log('navStatus', navStatus)
+	}
+  
+	const closeNav = ()=>{
+	  navStatus = false
+	}
+  
+	const linkStyle = 'block py-2 px-3 text-md hover:underline text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+	const activeStyle = 'block py-2 px-3 text-md hover:underline text-white bg-primary-700 rounded md:bg-transparent md:text-primary-700 md:p-0 dark:text-white md:dark:text-primary-500'
+  </script>
+  
+  <nav class="bg-transparent border-gray-200 dark:bg-gray-900">
+	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+	  <a href="/" onclick={closeNav} class="flex items-center space-x-3 rtl:space-x-reverse">
+		  <span class="self-center text-2xl sm:text-3xl text-primary-900 font-semibold whitespace-nowrap dark:text-white">FlexiLexi dictionary</span>
+	  </a>
+	  <button onclick={toggleNav} type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default">
+		  <span class="sr-only">Open main menu</span>
+		  <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+			  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+		  </svg>
+	  </button>
+	  <div class="{navClass} w-full md:block md:w-auto" id="navbar-default">
+		<ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+		  <li>
+			<a href="/" onclick={closeNav} aria-current={$page.url.pathname === '/'} class="{$page.url.pathname === '/' ? activeStyle : linkStyle}">Norsk</a>
+		  </li>
+		  <li>
+			<a href="/english" onclick={closeNav} aria-current={$page.url.pathname === '/english'} class="{$page.url.pathname === '/english' ? activeStyle : linkStyle}">English</a>
+		  </li>
+		  <li>
+			<a href="/japanese" onclick={closeNav} aria-current={$page.url.pathname === '/japanese'} class="{$page.url.pathname === '/japanese' ? activeStyle : linkStyle}">Japanese</a>
+		  </li>
+		</ul>
+	  </div>
+	</div>
+  
+  </nav>
