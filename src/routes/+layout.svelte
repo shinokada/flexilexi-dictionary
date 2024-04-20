d4bc3e7302887c476b917c66c57d9fce45f097e7
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-20 17:57:05 +0200

    feat: add runatics and runes-meta-tags, add home and about tests

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script>
   import "../app.pcss";
-	let { children } = $props();
+  import { Runatics } from 'runatics';
+	import { RunesMetaTags, deepMerge } from 'runes-meta-tags';
+  import { page } from '$app/stores';
+	let { children, data } = $props();
   import Nav from './components/Nav.svelte'
   import Footer from './components/Footer.svelte'
-  // import { Navbar, Footer } from '$lib'
+  let metaTags = $state(
+    $page.data.pageMetaTags
+      ? deepMerge($page.data.layoutMetaTags, $page.data.pageMetaTags)
+      : data.layoutMetaTags
+  );
+  $effect(() => {
+    metaTags = $page.data.pageMetaTags ? deepMerge($page.data.layoutMetaTags, $page.data.pageMetaTags ) : data.layoutMetaTags
+  });
+	const analyticsId = data.ANALYTICS_ID
 </script>
 
+<Runatics {analyticsId} />
+<RunesMetaTags {...metaTags}/>
 <Nav />
 
 <section class="min-h-screen">
